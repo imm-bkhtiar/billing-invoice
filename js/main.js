37e16f6134df87d15b9965dd0988c2c31b627555
imm-bkhtiar <bakhtiarimam801@gmail.com>
2025-01-29 19:12:32 +0700

    fix input format currency and relative input width

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-const prices = document.querySelectorAll(".invoice tbody tr td:nth-child(3)")
+const prices = document.querySelectorAll(
+  ".invoice tbody tr td:nth-child(3) input",
+);
+const inputName = document.querySelectorAll(
+  ".invoice tbody tr td input[name='name']",
+);
+const inputQuantity = document.querySelectorAll(
+  ".invoice tbody tr td input[name='quantity']",
+);
 
-const formatter = new Intl.NumberFormat('id-ID', {
-  style: 'currency',
-  currency: 'IDR',
-  minimumFractionDigits: 0
+console.log(inputName);
+console.log(inputQuantity);
+
+prices.forEach((price) => {
+  price.addEventListener("input", (e) => {
+    let value = e.target.value.replace(/[^0-9]/g, "");
+
+    const inputCursor = e.target.selectionStart;
+
+    const formatter = new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 0,
+    }).format(value ? parseInt(value) : 0);
+
+    e.target.value = formatter.replace();
+
+    e.target.style.width = e.target.textLength + "ch";
+    input.selectionStart = input.selectionEnd = inputCursor;
+    e.target.style.maxWidth = "20ch";
+  });
 });
-
-prices.forEach(( price ) => {
- const priceAfterFormat = formatter.format(parseInt(price.textContent));
-  price.textContent = priceAfterFormat 
-})
-
